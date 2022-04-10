ce41fb34c1469caad35f44cf9d719e740492a80e
Nikita <khakimov.nikita.vch@gmail.com>
2022-04-10 18:56:26 +0300

    omdbAPI

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,78 +1,64 @@
-import { useCallback, useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { useGetHeroesQuery, useDeleteHeroMutation } from '../../api/apiSlice';
+import { useCallback, useMemo } from "react";
+import { useSelector } from "react-redux";
+import { useGetHeroesQuery, useDeleteHeroMutation } from "../../api/apiSlice";
 
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import HeroesListItem from "../heroesListItem/HeroesListItem";
-import Spinner from '../spinner/Spinner';
+import Spinner from "../spinner/Spinner";
 
-import './heroesList.scss'
+import "./heroesList.scss";
 
 const HeroesList = () => {
+  const { data: heroes = [], isLoading, isError } = useGetHeroesQuery();
 
-	const {
-		data: heroes = [],
-		isLoading,
-		isError
-	} = useGetHeroesQuery()
+  const [deleteHero] = useDeleteHeroMutation();
 
-	const [deleteHero] = useDeleteHeroMutation();
+  const activeFilter = useSelector((state) => state.filters.activeFilter);
 
-	const activeFilter = useSelector(state => state.filters.activeFilter)
+  const filteredHeroes = useMemo(() => {
+    const filteredHeroes = heroes.slice();
+    if (activeFilter === "all") {
+      return filteredHeroes;
+    } else {
+      return filteredHeroes.filter((item) => item.element === activeFilter);
+    }
+  }, [heroes, activeFilter]);
 
-	const filteredHeroes = useMemo(() => {
-		const filteredHeroes = heroes.slice();
-		if (activeFilter === 'all') {
-			return filteredHeroes;
-		} else {
-			return filteredHeroes.filter(item => item.element === activeFilter);
-		}
-	}, [heroes, activeFilter])
+  const onDelete = useCallback((id) => {
+    deleteHero(id);
+  }, []);
 
-	const onDelete = useCallback((id) => {
-		deleteHero(id)
-	}, [])
+  if (isLoading) {
+    return <Spinner />;
+  } else if (isError) {
+    return <h5 className="text-center mt-5">Ошибка загрузки</h5>;
+  }
 
-	if (isLoading) {
-		return <Spinner />;
-	} else if (isError) {
-		return <h5 className="text-center mt-5">Ошибка загрузки</h5>
-	}
+  const renderHeroesList = (arr) => {
+    if (arr.length === 0) {
+      return (
+        <CSSTransition timeout={0} classNames="hero">
+          <h5 className="text-center mt-5">Героев пока нет</h5>
+        </CSSTransition>
+      );
+    }
+    return arr.map(({ id, ...props }) => {
+      return (
+        <CSSTransition key={id} timeout={500} classNames="hero">
+          <HeroesListItem {...props} onDelete={() => onDelete(id)} />
+        </CSSTransition>
+      );
+    });
+  };
 
-	const renderHeroesList = (arr) => {
-		if (arr.length === 0) {
-			return (
-				<CSSTransition
-					timeout={0}
-					classNames="hero">
-					<h5 className="text-center mt-5">Героев пока нет</h5>
-				</CSSTransition>
-			)
-		}
-		return arr.map(({ id, ...props }) => {
-			return (
-				<CSSTransition
-					key={id}
-					timeout={500}
-					classNames="hero"
-				>
-					<HeroesListItem
-						{...props}
-						onDelete={() => onDelete(id)} />
-				</CSSTransition>
-			)
-		})
-	}
+  const elements = renderHeroesList(filteredHeroes);
 
-	const elements = renderHeroesList(filteredHeroes);
-	return (
-		<ul>
-			<TransitionGroup>
-				{elements}
-			</TransitionGroup>
-		</ul>
-	)
-}
+  return (
+    <ul>
+      <TransitionGroup>{elements}</TransitionGroup>
+    </ul>
+  );
+};
 
 export default HeroesList;
